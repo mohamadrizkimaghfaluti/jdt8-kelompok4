61e3e956663147c8d92319cc00482b4b13c6671b
mohamadrizkimaghfaluti <79871663+mohamadrizkimaghfaluti@users.noreply.github.com>
2021-09-26 00:52:33 +0700

    add feature subject

--- back-end-university/MiniProjectUniversity/src/main/java/com/example/MiniProjectUniversity/controller/SubjectController.java
+++ back-end-university/MiniProjectUniversity/src/main/java/com/example/MiniProjectUniversity/controller/SubjectController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/subject")
@@ -22,9 +23,27 @@ public class SubjectController {
         return new ResponseEntity<>(subjectList, HttpStatus.OK);
     }
 
+    @GetMapping("getIdSubject/{id}")
+    public ResponseEntity<Optional> findById(@PathVariable String id) {
+        Optional optional = service.findById(id);
+        return new ResponseEntity<>(optional, HttpStatus.OK);
+    }
+
     @PostMapping("/create")
     public ResponseEntity<Subject> create(@RequestBody Subject subject){
         Subject subject1 = service.create(subject);
         return new ResponseEntity<>(subject1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Subject> update(@RequestBody Subject subject){
+        Subject subject1 = service.update(subject);
+        return new ResponseEntity<>(subject1, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> delete(@PathVariable String id){
+        Object data = service.delete(id);
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }

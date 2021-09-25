0d62b5dda3194042547e013dac7e8157c658ae21
ariaditafadllika <57275194+Ariaditaputrif@users.noreply.github.com>
2021-09-25 12:25:09 +0700

    no message

---
+++ back-end-university/MiniProjectUniversity/src/main/java/com/example/MiniProjectUniversity/model/Subject.java
@@ -0,0 +1,2 @@
+package com.example.MiniProjectUniversity.model;public class Subject {
+}
---
+++ back-end-university/MiniProjectUniversity/src/main/java/com/example/MiniProjectUniversity/repository/SubjectRepository.java
@@ -0,0 +1,2 @@
+package com.example.MiniProjectUniversity.repository;public interface SubjectRepository {
+}
